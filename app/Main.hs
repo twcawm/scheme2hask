b5ad3bdc57a9e81228e530007f3977a697cafa0c
twcawm <twcawm@gmail.com>
2022-08-09 15:29:24 -0400

    add more parsers for Scheme atoms, strings, and basic expressions

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,53 @@ module Main where
 import Lib
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x --String x constructs a LispVal, applying return to this creates a Parser LispVal
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest --use cons instead of ++ since first is just a char
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _ -> Atom atom --underscore here matches anything (wildcard)
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit  --parse many digits.  apply "read" to this
+--pass the result to Number constructor.  
+--we want this all to be inside of the Parser monad, (many1 digit produces a Parser String, not a String)
+--therefore liftM lifts this function into monad form
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom --accept any of the following parsed types
+        <|> parseString
+        <|> parseNumber
+
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
+
+data LispVal = Atom String
+            | List [LispVal]
+            | DottedList [LispVal] LispVal
+            | Number Integer
+            | String String
+            | Bool Bool
 
 main :: IO ()
 main = do
